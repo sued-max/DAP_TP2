f018cc046f77ce358b937878367b6f54414d38c8
sued-max <49434328@est.ort.edu.ar>
2026-06-10 18:46:14 -0300

    Create product.dart

---
+++ product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String name;
+  final String description;
+  final double price;
+  final int quantity;
+
+  Product({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.quantity,
+  });
+}
